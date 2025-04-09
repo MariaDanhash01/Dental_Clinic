36b458a01fb92df8db647decff7e03815f7d085c
Ebrahem Alwish <ebrahemk968@gmail.com>
2025-04-09 14:11:20 +0300

    Store Keeper Medical Supplies

--- resources/views/Layouts/StoreKeeperEmployee/Sidebar.blade.php
+++ resources/views/Layouts/StoreKeeperEmployee/Sidebar.blade.php
@@ -1,49 +1,72 @@
 <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
     <ul id="sidebarnav">
-      <li class="nav-small-cap">
-        <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-        <span class="hide-menu">Home</span>
-      </li>
-      <li class="sidebar-item">
-        <a class="sidebar-link" href="{{route('storeKeeperEmployee.index')}}" aria-expanded="false">
-          <span>
-            <i class="ti ti-layout-dashboard"></i>
-          </span>
-          <span class="hide-menu">Dashboard</span>
-        </a>
-      </li>
-      
-      <li class="nav-small-cap">
-        <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-        <span class="hide-menu">UI COMPONENTS</span>
-      </li>
+        <li class="nav-small-cap">
+            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+            <span class="hide-menu">Home</span>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('storeKeeperEmployee.index') }}" aria-expanded="false">
+                <span>
+                    <i class="ti ti-layout-dashboard"></i>
+                </span>
+                <span class="hide-menu">Dashboard</span>
+            </a>
+        </li>
 
-      <li class="sidebar-item">
-        <details>
-          <summary class="sidebar-link">
-            <span>
-              <i class="ti ti-stethoscope"></i>
-            </span>
-            <span class="hide-menu">Doctors</span>
-          </summary>
-          <ul style="padding-left: 1rem; list-style: none;">
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="{{route('admin.doctor.index')}}">
-                <span class="hide-menu">Doctor Table</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="{{route('admin.doctor.create')}}">
-                <span class="hide-menu">Create Doctor</span>
-              </a>
-            </li>
-          </ul>
-        </details>
-      </li>
-     
+        <li class="nav-small-cap">
+            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+            <span class="hide-menu">Services</span>
+        </li>
+
+        <style>
+            .sidebar-link {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                cursor: pointer;
+            }
+
+            .arrow-icon {
+                transition: transform 0.3s ease;
+            }
+
+            .sidebar-details[open] .arrow-icon {
+                transform: rotate(180deg);
+            }
+        </style>
+
+        <li class="sidebar-item">
+            <details class="sidebar-details">
+                <summary class="sidebar-link">
+                    <span>
+                      <i class="ti ti-first-aid-kit"></i>
+
+                    </span>
+                    <span class="hide-menu">Medical Supplies</span>
+                    <i class="ti ti-chevron-down arrow-icon"></i>
+                </summary>
+                <ul style="padding-left: 1rem; list-style: none;">
+                   
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="{{ route('storeKeeperEmployee.medicalSupplies.index') }}">
+                            <span class="hide-menu">Medical Supplies Table</span>
+                        </a>
+                    </li>
+
+                    <li class="sidebar-item">
+                        <a class="sidebar-link" href="{{ route('storeKeeperEmployee.medicalSupplies.create') }}">
+                            <span class="hide-menu">Create Medical Supplies</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </details>
+        </li>
+
+
     </ul>
-  
-{{--     
+
+    {{--     
 <!-- تضمين Bootstrap JS (مطلوب لتفعيل الـ collapse) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> --}}
-  </nav>
+</nav>
